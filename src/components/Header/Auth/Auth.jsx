e699696cf2d26b42268aca9dcca4ed490280aa6c
Yan Shatskiy <60981097+jan-web@users.noreply.github.com>
2024-01-14 12:56:44 +0100

    feat: The tasks of lesson 6 done

--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -1,14 +1,69 @@
 import style from './Auth.module.css';
 import {ReactComponent as LoginIcon} from './img/login.svg';
 import PropTypes from 'prop-types';
+import {urlAuth} from '../../../api/auth';
+import {Text} from '../../../UI/Text';
+import {useEffect, useState} from 'react';
+import {URL_API} from '../../../api/const';
 
-export const Auth = ({auth}) => (
-  <button className={style.button}>
-    {' '}
-    {auth ? auth : <LoginIcon className={style.svg} />}{' '}
-  </button>
-);
+export const Auth = ({token, delToken}) => {
+  const [auth, setAuth] = useState({});
+  const [logOutVisible, setLogOutVisible] = useState(false);
 
+  useEffect(() => {
+    if (!token) return;
+
+    fetch(`${URL_API}/api/v1/me`, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(({name, icon_img: iconImg}) => {
+        const img = iconImg.replace(/\?.*$/, '');
+        setAuth({name, img});
+      })
+      .catch((err) => {
+        console.log(err);
+        setAuth({});
+      });
+  }, [token]);
+
+  const handleShowLogOut = () => {
+    setLogOutVisible(!logOutVisible);
+  };
+
+  const handleSignOut = () => {
+    setAuth({});
+    delToken();
+  };
+  return (
+    <div className={style.container}>
+      {auth.name ? (
+        <>
+          <button className={style.btn} onClick={handleShowLogOut}>
+            <img
+              className={style.img}
+              src={auth.img}
+              title={auth.name}
+              alt={auth.name}
+            />
+          </button>
+          {logOutVisible && (
+            <button className={style.logout} onClick={handleSignOut}>
+              Выйти
+            </button>
+          )}
+        </>
+      ) : (
+        <Text className={style.authLink} As="a" href={urlAuth}>
+          <LoginIcon className={style.svg} />
+        </Text>
+      )}
+    </div>
+  );
+};
 Auth.propTypes = {
-  auth: PropTypes.bool,
+  token: PropTypes.string,
+  delToken: PropTypes.func,
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,25 @@
-
 import Layout from '../Layout';
 import Auth from './Auth';
 import style from './Header.module.css';
 import Heading from './Heading';
 import Logo from './Logo';
 import Search from './Search';
+import PropTypes from 'prop-types';
 
-export const Header = () => (
+export const Header = ({token, delToken}) => (
   <header className={style.header}>
     <Layout>
       <div className={style.gridContainer}>
         <Logo />
-        <Heading text={'Главная'}/>
+        <Heading text={'Главная'} />
         <Search />
-        <Auth auth={false}/>
+        <Auth token={token} delToken={delToken}/>
       </div>
     </Layout>
   </header>
 );
+
+Header.propTypes = {
+  token: PropTypes.string,
+  delToken: PropTypes.func,
+};
